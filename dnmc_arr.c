84d6cd886b51d345585a5cfa1759dda9ef40eb7e
Suo Desheng <1547252584@qq.com>
2026-03-13 15:24:48 +0800

    Big Update

--- dnmc_arr.c
+++ dnmc_arr.c
@@ -4,222 +4,358 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <stdint.h>
+#include <stdalign.h>
+
+#define DA_FAST_MODE DARR_FAST_MODE
+
+#define DA_STAT_EMPTY DARR_STAT_EMPTY
+#define DA_STAT_OK DARR_STAT_OK
+#define DA_STAT_MOVED DARR_STAT_MOVED
+#define DA_STAT_FAIL DARR_STAT_FAIL
+
+#define DA_FUNC_OK DARR_FUNC_OK
+#define DA_FUNC_FAIL DARR_FUNC_FAIL
+ 
+_Static_assert(DARR_SIZE_MAX_CALC <= SIZE_MAX,
+    "DARR_SIZE_MAX should not exceed SIZE_MAX.\n");
+#define DA_SIZE_MAX DARR_SIZE_MAX_CALC
+#define DA_STAT_LEN_MAX DARR_BITS_MAX_CALC / 2
+
+#define DA_ALIGN alignof(max_align_t)
+
+#define DA_MAX_ALLOC ((DA_SIZE_MAX >> 1) + 1)
 
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
-_Static_assert(DA_INIT_ALLOC >= 64,
-    "DA_INIT_ALLOC shouldn't less than 64");
-
-_Static_assert((DA_INIT_ALLOC & (DA_INIT_ALLOC - 1)) == 0,
-    "DA_INIT_ALLOC should be a power of 2");
+static inline size_t pow2_next(size_t n) {
+    assert(n <= DA_MAX_ALLOC);
+    if(n <= 1) {
+        return 1;
+    }
+    n--;
+    n |= n >> 1;
+    n |= n >> 2;
+    n |= n >> 4;
+    #if DA_SIZE_MAX >= UINT16_MAX
+        n |= n >> 8;
+    #if DA_SIZE_MAX >= UINT32_MAX
+        n |= n >> 16;
+    #if DA_SIZE_MAX >= UINT64_MAX
+        n |= n >> 32;
+    #endif
+    #endif
+    #endif
+    return n + 1;
+}
 
 typedef struct {
     size_t len;
-    size_t alloc;
     size_t esize;
-    int stat;
+    size_t alloc;
+    void *body;
+    size_t stat;
+} header_base;
+
+#define DA_PAD_SIZE ((DA_ALIGN - (sizeof(header_base) % DA_ALIGN)) % DA_ALIGN)
+
+typedef struct {
+    size_t len;
+    size_t esize;
+    size_t alloc;
+    void *body;
+    size_t stat;
+    unsigned char pad[DA_PAD_SIZE];
 } header;
 
-static header* hdr_get(void* darr) {
+#define DA_MIN_ALLOC (pow2_next(sizeof(header)))
+
+typedef struct {
+    void *value;
+    size_t index;
+} data_in;
+
+static inline header* hdr_get(void* darr) {
     return (header*)darr - 1;
 }
 
-static void* darr_get(header* hdr) {
+static inline void* darr_get(header* hdr) {
     return (void*)(hdr + 1);
 }
 
+static inline size_t len_get(size_t alloc, size_t esize) {
+    return (alloc - sizeof(header)) / esize;
+}
+
 void* darr_init(size_t esize) {
-    if(esize == 0) {
+    if(esize == 0 || esize > DA_MAX_ALLOC) {
         return NULL;
     }
-    header *hdr = (header*)malloc(DA_INIT_ALLOC);
+    header *hdr = (header*)malloc(DA_MIN_ALLOC);
     if(hdr == NULL) {
         return NULL;
     }
+    void *darr = darr_get(hdr);
     hdr->len = 0;
-    hdr->alloc = DA_INIT_ALLOC;
+    hdr->alloc = DA_MIN_ALLOC;
     hdr->esize = esize;
+    hdr->body = darr;
     hdr->stat = 0;
-    return darr_get(hdr);
-}
-
-static int hdr_test(header* hdr) {
-    if(hdr->len > (hdr->alloc - sizeof(header)) / hdr->esize) {
-        return 0;
-    } else if((hdr->alloc & (hdr->alloc - 1)) != 0) {
-        return 0;
-    } else if(hdr->alloc < DA_INIT_ALLOC) {
-        return 0;
-    } else if(hdr->stat < -1 || hdr->stat > 1) {
-        return 0;
-    }
-    return 1;
-}
-
-static int darr_test(void* darr) {
-    if((uintptr_t)darr % sizeof(void*) != 0) {
-        return 0;
-    }
-    if(darr == NULL) {
-        return 0;
-    }
-    if(!hdr_test(hdr_get(darr))) {
-        return 0;
-    }
-    return 1;
-}
-
-static size_t total_get(size_t esize, size_t len) {
-    return sizeof(header) + esize * len;
-}
-
-static header* hdr_update(header* hdr, size_t len) {
-    if(len > (SIZE_MAX - sizeof(header)) / hdr->esize) {
-        hdr->stat = -1;
-        return NULL;
-    }
-    size_t total = MAX(total_get(hdr->esize, len), DA_INIT_ALLOC);
-    size_t alloc = hdr->alloc;
-    while(alloc < total) {
-        alloc *= 2;
-    }
-    while((alloc / 2) > total) {
-        alloc /= 2;
-    }
-    header *new_hdr = (header*)realloc((void*)hdr, alloc);
-    if(new_hdr == NULL) {
-        hdr->stat = -1;
-        return NULL;
-    }
-    new_hdr->len = len;
-    new_hdr->alloc = alloc;
-    new_hdr->esize = hdr->esize;
-    new_hdr->stat = (new_hdr != hdr) ? 1 : 0;
-    return new_hdr;
-}
-
-void* darr_setlen(void* darr, size_t len) {
-    if(!darr_test(darr)) {
-        return NULL;
-    }
-    header *hdr = hdr_get(darr);
-    if(len == hdr->len) {
-        hdr->stat = 0;
-        return darr;
-    }
-    header *new_hdr = hdr_update(hdr, len);
-    if(new_hdr == NULL) {
-        return darr;
-    }
-    return darr_get(new_hdr);
-}
-
-void* darr_addlen(void* darr, size_t len) {
-    if(!darr_test(darr)) {
-        return NULL;
-    }
-    header *hdr = hdr_get(darr);
-    if(len == 0) {
-        hdr->stat = 0;
-        return darr;
-    }
-    if(len > SIZE_MAX - hdr->len) {
-        hdr->stat = -1;
-        return darr;
-    }
-    return darr_setlen(darr, hdr->len + len);
-}
-
-void* darr_clear(void* darr) {
-    if(!darr_test(darr)) {
-        return NULL;
-    }
-    header *hdr = (header*)darr - 1;
-    void *new_darr = realloc(hdr, DA_INIT_ALLOC);
-    if(new_darr == NULL) {
-        hdr->stat = -1;
-        return darr;
-    }
-    header *new_hdr = hdr_get(new_darr);
-    new_hdr->len = 0;
-    new_hdr->alloc = DA_INIT_ALLOC;
-    new_hdr->stat = (new_darr != darr) ? 1 : 0;
-    new_hdr->esize = hdr->esize;
-    return new_darr;
-}
-
-void* darr_reset(void* darr, size_t esize) {
-    if(esize == 0) {
-        return NULL;
-    }
-    void *new_darr = darr_clear(darr);
-    if(new_darr == NULL) {
-        return NULL;
-    }
-    hdr_get(new_darr)->esize = esize;
-    return new_darr;
-}
-
-static void* darr_add(void* darr, void* data) {
-    header *hdr = hdr_get(darr);
-    void *ptr = (unsigned char*)darr + hdr->esize * (hdr->len - 1);
-    memcpy(ptr, data, hdr->esize);
     return darr;
 }
 
-void* darr_push(void* darr, void* data) {
-    if(!darr_test(darr)) {
+#ifdef DA_FAST_MODE
+#   define darr_test(d) 1
+static inline int darr_fast_test(void* darr) {
+    if(darr == NULL) {
+        return 0;
+    }
+    if((uintptr_t)darr % DA_ALIGN != 0) {
+        return 0;
+    }
+    header *hdr = hdr_get(darr);
+    if(hdr == NULL) {
+        return 0;
+    }
+    if((uintptr_t)hdr % DA_ALIGN != 0) {
+        return 0;
+    }
+    return 1;
+}
+#else
+#   define darr_fast_test(d) 1
+static inline int hdr_mat(header* hdr) {
+    if(hdr == NULL) {
+        return 0;
+    }
+    if((uintptr_t)hdr % DA_ALIGN != 0) {
+        return 0;
+    }
+    if(hdr->body != darr_get(hdr)) {
+        return 0;
+    }
+    return 1;
+}
+
+static inline int hdr_vld(header* hdr) {
+    if((hdr->alloc & (hdr->alloc - 1)) != 0) {
+        return 0;
+    }
+    if(hdr->alloc < DA_MIN_ALLOC || hdr->alloc > DA_MAX_ALLOC) {
+        return 0;
+    }
+    if(hdr->esize == 0) {
+        return 0;
+    }
+    if(hdr->len > len_get(hdr->alloc, hdr->esize)) {
+        return 0;
+    }
+    return 1;
+}
+
+static inline int darr_mat(void* darr) {
+    if(darr == NULL) {
+        return 0;
+    }
+    if((uintptr_t)darr % DA_ALIGN != 0) {
+        return 0;
+    }
+    return 1;
+}
+
+static inline int darr_test(void* darr) {
+    if(!darr_mat(darr)) {
+        return 0;
+    }
+    header *hdr = hdr_get(darr);
+    if(!hdr_mat(hdr)) {
+        return 0;
+    }
+    if(!hdr_vld(hdr)) {
+        return 0;
+    }
+    return 1;
+}
+#endif
+
+static inline int update_arg_mat(header* hdr, size_t len, size_t esize, data_in *data) {
+    if(esize == 0 || esize > DA_MAX_ALLOC) {
+        return 0;
+    }
+    if(esize != hdr->esize && len != 0) {
+        return 0;
+    }
+    if(len > len_get(DA_MAX_ALLOC, esize)) {
+        return 0;
+    }
+    if(data != NULL && (data->value == NULL || data->index >= len)) {
+        return 0;
+    }
+    return 1;
+}
+
+static inline void stat_push(size_t *stat, size_t code) {
+    *stat <<= 2;
+    *stat |= code;
+}
+
+static void* darr_update(void *darr, size_t len, size_t esize, data_in *data) {
+    header *hdr = hdr_get(darr);
+    if(!update_arg_mat(hdr, len, esize, data)) {
+        stat_push(&hdr->stat, DA_STAT_FAIL);
+        return darr;
+    }
+    if(len == hdr->len && data == NULL) {
+        stat_push(&hdr->stat, DA_STAT_OK);
+        return darr;
+    }
+    size_t alloc = hdr->alloc;
+    size_t target = MAX(sizeof(header) + len * esize, DA_MIN_ALLOC);
+    while(alloc < target) {
+        alloc *= 2;
+    }
+    while((alloc / 2) > target) {
+        alloc /= 2;
+    }
+    size_t stat_bk = hdr->stat;
+    header *new_hdr = (header*)realloc((void*)hdr, alloc);
+    if(new_hdr == NULL) {
+        stat_push(&hdr->stat, DA_STAT_FAIL);
+        return darr;
+    }
+    void *new_darr = darr_get(new_hdr);
+    new_hdr->len = len;
+    new_hdr->alloc = alloc;
+    new_hdr->esize = esize;
+    new_hdr->body = new_darr;
+    new_hdr->stat = stat_bk;
+    if(new_hdr != hdr) {
+        stat_push(&new_hdr->stat, DA_STAT_MOVED);
+    } else {
+        stat_push(&new_hdr->stat, DA_STAT_OK);
+    }
+    if(data != NULL) {
+        void *ptr = (unsigned char*)new_darr + new_hdr->esize * data->index;
+        memcpy(ptr, data->value, new_hdr->esize);
+    }
+    return new_darr;
+}
+
+static inline int addlen_safe(header* hdr, size_t len) {
+    size_t max_len = len_get(DA_MAX_ALLOC, hdr->esize);
+    if(hdr->len > max_len) {
+        return 0;
+    }
+    if(len > max_len - hdr->len) {
+        return 0;
+    }
+    return 1;
+}
+
+void* darr_setlen(void* darr, size_t len) {
+    if(!darr_test(darr) || !darr_fast_test(darr)) {
+        return NULL;
+    }
+    return darr_update(darr, len, hdr_get(darr)->esize, NULL);
+}
+
+void* darr_addlen(void* darr, size_t len) {
+    if(!darr_test(darr) || !darr_fast_test(darr)) {
         return NULL;
     }
     header *hdr = hdr_get(darr);
-    size_t total = total_get(hdr->esize, hdr->len + 1);
-    if(hdr->alloc >= total) {
-        hdr->stat = 0;
-        hdr->len++;
-        return darr_add(darr, data);
-    }
-    void *new_darr = darr_addlen(darr, 1);
-    if(hdr_get(new_darr)->stat == -1) {
-        hdr->stat = -1;
+    if(!addlen_safe(hdr, len)) {
+        stat_push(&hdr->stat, DA_STAT_FAIL);
         return darr;
     }
-    return darr_add(new_darr, data);
+    return darr_update(darr, hdr->len + len, hdr->esize, NULL);
 }
 
-int darr_stat(void* darr) {
-    if(!darr_test(darr)) {
-        return -2;
+void* darr_clear(void* darr) {
+    if(!darr_test(darr) || !darr_fast_test(darr)) {
+        return NULL;
     }
-    return hdr_get(darr)->stat;
+    return darr_update(darr, 0, hdr_get(darr)->esize, NULL);
 }
 
-long long darr_tot(void* darr) {
-    if(!darr_test(darr)) {
-        return -2;
+void* darr_reset(void* darr, size_t esize) {
+    if(!darr_test(darr) || !darr_fast_test(darr)) {
+        return NULL;
     }
-    header *hdr = hdr_get(darr);
-    return (long long)((hdr->alloc - sizeof(header)) / hdr->esize);
+    return darr_update(darr, 0, esize, NULL);
 }
 
-long long darr_len(void* darr) {
-    if(!darr_test(darr)) {
-        return -2;
-    }
-    return (long long)(hdr_get(darr)->len);
-}
-
-long long darr_rem(void* darr) {
-    if(!darr_test(darr)) {
-        return -2;
+void* darr_push(void* darr, void* data) {
+    if(!darr_test(darr) || !darr_fast_test(darr)) {
+        return NULL;
     }
     header *hdr = hdr_get(darr);
-    return (long long)((hdr->alloc - sizeof(header)) / hdr->esize - hdr->len);
+    if(!addlen_safe(hdr, 1)) {
+        stat_push(&hdr->stat, DA_STAT_FAIL);
+        return darr;
+    }
+    data_in din = {data, hdr->len};
+    return darr_update(darr, hdr->len + 1, hdr->esize, &din);
+}
+
+static inline int buf_arg_mat(const char* buf, size_t len) {
+    if(len < 2 || buf == NULL) {
+        return 0;
+    }
+    return 1;
+}
+
+char* darr_stat(void* darr, char* buf, size_t len) {
+    if(!buf_arg_mat(buf, len) || !darr_test(darr) || !darr_fast_test(darr)) {
+        return NULL;
+    }
+    size_t init = (len > DA_STAT_LEN_MAX + 1) ? len - DA_STAT_LEN_MAX - 1 : 0;
+    for(int i = 0; i < (int)init; i++) {
+        buf[i] = '0';
+    }
+    size_t stat = hdr_get(darr)->stat;
+    for(int i = (int)len - 2; i >= (int)init; i--) {
+        buf[i] = '0' + (stat & 3);
+        stat >>= 2;
+    }
+    buf[len-1] = '\0';
+    return buf;
+}
+
+int darr_stat_clear(void* darr) {
+    if(!darr_test(darr) || !darr_fast_test(darr)) {
+        return DA_FUNC_FAIL;
+    }
+    hdr_get(darr)->stat = 0;
+    return DA_FUNC_OK;
+}
+
+int64_t darr_tot(void* darr) {
+    if(!darr_test(darr) || !darr_fast_test(darr)) {
+        return DA_FUNC_FAIL;
+    }
+    header *hdr = hdr_get(darr);
+    return (int64_t)len_get(hdr->alloc, hdr->esize);
+}
+
+int64_t darr_len(void* darr) {
+    if(!darr_test(darr) || !darr_fast_test(darr)) {
+        return DA_FUNC_FAIL;
+    }
+    return (int64_t)(hdr_get(darr)->len);
+}
+
+int64_t darr_rem(void* darr) {
+    if(!darr_test(darr) || !darr_fast_test(darr)) {
+        return DA_FUNC_FAIL;
+    }
+    header *hdr = hdr_get(darr);
+    return (int64_t)(len_get(hdr->alloc, hdr->esize) - hdr->len);
 }
 
 int darr_free(void* darr) {
-    if(!darr_test(darr)) {
-        return -2;
+    if(!darr_test(darr) || !darr_fast_test(darr)) {
+        return DA_FUNC_FAIL;
     }
     free(hdr_get(darr));
-    return 0;
+    return DA_FUNC_OK;
 }
--- dnmc_arr.h
+++ dnmc_arr.h
@@ -1,11 +1,41 @@
 #ifndef DA_DNMC_ARR_H
 #define DA_DNMC_ARR_H
 
+#include <stdint.h>
 #include <stddef.h>
+#include <limits.h>
 
-#ifndef DA_INIT_ALLOC
-#define DA_INIT_ALLOC 64
+#ifdef DARR_FAST
+#   define DARR_FAST_MODE 1
+#else
+#   define DARR_FAST_MODE 0
 #endif
+
+#ifndef DARR_SIZE_MAX_64
+#ifndef DARR_SIZE_MAX_32
+#ifndef DARR_SIZE_MAX_16
+#   define DARR_SIZE_MAX_CALC SIZE_MAX
+#   define DARR_BITS_MAX_CALC (sizeof(size_t) * 8)
+#else
+#   define DARR_SIZE_MAX_CALC UINT16_MAX
+#   define DARR_BITS_MAX_CALC 16
+#endif
+#else
+#   define DARR_SIZE_MAX_CALC UINT32_MAX
+#   define DARR_BITS_MAX_CALC 32
+#endif
+#else
+#   define DARR_SIZE_MAX_CALC UINT64_MAX
+#   define DARR_BITS_MAX_CALC 64
+#endif
+
+#define DARR_STAT_EMPTY 0
+#define DARR_STAT_OK 1
+#define DARR_STAT_MOVED 2
+#define DARR_STAT_FAIL 3
+
+#define DARR_FUNC_OK 0
+#define DARR_FUNC_FAIL -1
 
 void* darr_init(size_t esize);
 void* darr_setlen(void* darr, size_t len);
@@ -13,10 +43,11 @@ void* darr_addlen(void* darr, size_t len);
 void* darr_clear(void* darr);
 void* darr_reset(void* darr, size_t esize);
 void* darr_push(void* darr, void* data);
-int darr_stat(void* darr);
-long long darr_tot(void* darr);
-long long darr_len(void* darr);
-long long darr_rem(void* darr);
+char* darr_stat(void* darr, char* buf, size_t len);
+int darr_stat_clear(void* darr);
+int64_t darr_tot(void* darr);
+int64_t darr_len(void* darr);
+int64_t darr_rem(void* darr);
 int darr_free(void* darr);
 
 #endif
